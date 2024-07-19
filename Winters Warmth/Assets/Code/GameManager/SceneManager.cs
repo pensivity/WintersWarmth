21f4ae3cf735b42f3bda48d54b1e9c82e31d2bb5
Cambell <cambellritchie@gmail.com>
2024-07-20 11:40:52 +1200

    Adding Game Manager and related scripts

--- Winters Warmth/Assets/Code/GameManager/SceneManager.cs
+++ Winters Warmth/Assets/Code/GameManager/SceneManager.cs
@@ -1,11 +1,7 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class SceneManager : MonoBehaviour
 {
-    public int carryCapacity;
-    public int houseWarmth;
-    public int playerWarmth;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
