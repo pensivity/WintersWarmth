57667e7dfe435327e7460d2b2e8065a08d6bc468
Cambell Ritchie <145305082+pensivity@users.noreply.github.com>
2024-07-20 14:17:01 +1200

    Scene setup (#3)
    * Adding Game Manager and related scripts
    
    * Added player movement. Need to add sprites etc.

--- Winters Warmth/Assets/Code/Player/PlayerMovement.cs
+++ Winters Warmth/Assets/Code/Player/PlayerMovement.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector3 input;
+
+    [SerializeField] private PlayerController player;
+
+
+    private void Awake()
     {
-        
+        // Get the character controller script
+        player = gameObject.GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Call it in update so timing doesn't mess with input
+        GetPlayerInput();
+
+        // Move the character
+        PlayerMove();
+    }
+
+    void GetPlayerInput()
+    {
+        // Get (normalised) player direction
+        input = new Vector3(UnityEngine.Input.GetAxis("Horizontal"), UnityEngine.Input.GetAxis("Vertical")).normalized;
+
+        // increase by speed
+        input *= player.speed;
+    }
+
+    void PlayerMove()
+    {        
+        // move the character
+        player.transform.position += input * Time.deltaTime;
     }
 }
