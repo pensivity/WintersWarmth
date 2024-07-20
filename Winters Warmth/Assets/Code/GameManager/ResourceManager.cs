de0d25938996d65ce9c8dad1710c2e1e63a0c0d7
Cambell Ritchie <145305082+pensivity@users.noreply.github.com>
2024-07-20 21:10:12 +1200

    Level work (#9)
    * Adding Game Manager and related scripts
    
    * Added player movement. Need to add sprites etc.
    
    * Adding sprites
    
    * Added house and level prefabs.
    
    * Adjusted prefabs
    
    * Interactible objects!
    
    * House Warmth!

--- Winters Warmth/Assets/Code/GameManager/ResourceManager.cs
+++ Winters Warmth/Assets/Code/GameManager/ResourceManager.cs
@@ -1,16 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] public int houseWarmth;
+
+    private bool isCooling;
+    private PlayerController player;
+    private int addedWarmth;
+
+    private void Awake()
     {
-        
+        houseWarmth = 100;
+        isCooling = false;
+        player = GameObject.Find("PlayerController").GetComponent<PlayerController>();
+        addedWarmth = 20;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // FixedUpdate is called once per set time
+    void FixedUpdate()
     {
-        
+        StartCoroutine(DecreaseWarmth());
+    }
+
+    IEnumerator DecreaseWarmth ()
+    {
+        if (!isCooling)
+        {
+            isCooling = true;
+            houseWarmth--;
+            yield return new WaitForSeconds(3);
+            isCooling = false;
+            yield return null;
+        }
+    }
+
+    public void AddFuel(Component sender, object data)
+    {
+        houseWarmth += player.carryCapacity * addedWarmth;
+        player.carryCapacity = 0;
     }
 }
