5ae9e3e3feb8b34f467a2b963385a8622e067a73
Cambell <cambellritchie@gmail.com>
2024-07-20 19:10:58 +1200

    Adjusted prefabs

--- Winters Warmth/Assets/Code/Player/PlayerController.cs
+++ Winters Warmth/Assets/Code/Player/PlayerController.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float speed;
+    [SerializeField] public int carryCapacity;
+    [SerializeField] public int playerWarmth;
 
 
     private void Awake()
     {
         speed = 5f;
+        carryCapacity = 5;
+        playerWarmth = 100;
     }
 
 
